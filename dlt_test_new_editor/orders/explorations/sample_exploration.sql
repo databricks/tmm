797161eda82e4cc9439d9d3f51f81521b6db4374
fmunz <fmunz>
2025-04-30 13:26:27 +0000

    first commit

---
+++ dlt_test_new_editor/orders/explorations/sample_exploration.sql
@@ -0,0 +1,8 @@
+-- Databricks notebook source
+-- This is an exploratory notebook. It is not executed as part of the pipeline.
+-- Use it to explore data produced by the pipeline.
+
+USE CATALOG `demo_frank`;
+USE SCHEMA `test`;
+
+SELECT * from sample_trips_orders;
