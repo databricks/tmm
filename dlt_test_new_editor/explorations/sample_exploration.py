557ce4293f8ea80d5eb9e51448ef9ff74ceef060
fmunz <fmunz>
2025-04-30 10:24:43 +0000

    xxx

--- dlt_test_new_editor/explorations/sample_exploration.py
+++
@@ -1,7 +0,0 @@
-# Databricks notebook source
-# This is an exploratory notebook. It is not executed as part of the pipeline.
-# Use it to explore data produced by the pipeline.
-#
-# You can freely use SQL and Python here, just as you do in the pipeline's .sql and .py files.
-
-spark.sql("SELECT * FROM dlt_test.test.sample_trips_apr_29_843")
--- dlt_test_new_editor/transformations/next_transformation.sql
+++
@@ -1,3 +0,0 @@
-CREATE MATERIALIZED VIEW new_transformation AS
-SELECT *
-FROM dlt_test.test.sample_zones
