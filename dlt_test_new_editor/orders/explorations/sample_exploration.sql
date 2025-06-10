a12cf6102938dc4726725c3ff26a55716e1fd344
Frank <25545442+fmunz@users.noreply.github.com>
2025-06-10 10:30:16 -0700

    Delete dlt_test_new_editor directory

--- dlt_test_new_editor/orders/explorations/sample_exploration.sql
+++
@@ -1,8 +0,0 @@
--- Databricks notebook source
--- This is an exploratory notebook. It is not executed as part of the pipeline.
--- Use it to explore data produced by the pipeline.
-
-USE CATALOG `demo_frank`;
-USE SCHEMA `test`;
-
-SELECT * from sample_trips_orders;
--- dlt_test_new_editor/orders/transformations/sample_zones_mv.sql
+++
@@ -1,7 +0,0 @@
--- This file defines a sample transformation.
--- Edit the sample below or add new transformations
--- using "+ Add" in the file browser.
-
-CREATE MATERIALIZED VIEW new_transformation AS
-SELECT *
-FROM demo_frank.test.sample_trips_orders
