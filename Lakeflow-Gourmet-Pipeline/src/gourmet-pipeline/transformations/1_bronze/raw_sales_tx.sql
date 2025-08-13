8889b346fb5a1fdec861ed2d45c2fa87a140048f
fmunz <fmunz>
2025-08-13 13:52:22 +0000

    fixes

--- Lakeflow-Gourmet-Pipeline/src/gourmet-pipeline/transformations/1_bronze/raw_sales_tx.sql
+++ Lakeflow-Gourmet-Pipeline/src/gourmet-pipeline/transformations/1_bronze/raw_sales_tx.sql
@@ -1,4 +1,10 @@
 CREATE STREAMING TABLE raw_sales_tx
+
+--  add a data quality expectations with AI and a sloppy sentence:
+--  "add data quality reasonable totalPrice less than 1000 and positive"
+
+--- ( CONSTRAINT reasonable_totalPrice EXPECT (totalPrice > 0 AND totalPrice < 1000))
+
 TBLPROPERTIES ("quality" = "bronze")
 COMMENT "streaming table for sales tx"
 AS SELECT
