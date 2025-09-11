17f469b3855ce8ea35ebd54341076c0f49207876
fmunz <fmunz>
2025-09-11 09:10:35 +0000

    new events environ

--- Lakeflow-Gourmet-Pipeline/misc/setup UC.sql
+++ Lakeflow-Gourmet-Pipeline/misc/setup UC.sql
@@ -1,2 +1,3 @@
+-- CREATE SCHEMA daiwt_gourmet ;
 GRANT CREATE SCHEMA, USE SCHEMA, USE CATALOG on CATALOG daiwt_gourmet TO `account users`; 
 GRANT USAGE, SELECT, EXECUTE ON SCHEMA daiwt_gourmet.virtual_event TO `account users`; 
